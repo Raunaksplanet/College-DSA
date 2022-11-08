bbebf61734b406884f71e398d511d51eed332a12
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-11-08 18:05:48 +0530

    Create three ways to insertion in array.c

---
+++ three ways to insertion in array.c
@@ -0,0 +1,10 @@
+/*
+three ways to insertion in array:- 1) Insert in beginning 
+                                   2) insert at the end of array
+                                   3) insert new element at given position.
+1) Insert in beginning
+*/
+
+/* 
+
+*/
