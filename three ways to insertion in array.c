388ffec835887bd1ec8582752017a543f89595cd
Raunak Gupta <111748076+Raunaksplanet@users.noreply.github.com>
2022-11-08 18:16:39 +0530

    Update three ways to insertion in array.c

--- three ways to insertion in array.c
+++ three ways to insertion in array.c
@@ -5,6 +5,27 @@ three ways to insertion in array:- 1) Insert in beginning
 1) Insert in beginning
 */
 
-/* 
 
+
+
+
+
+
+
+
+
+/* 
+#1. Algorithm for array traversal 
+Arr[] - array varible 
+N - total number of element
+data - varible for inserting new num
+
+step 1 Initialize var I=n
+step 2 while(I<0) Repeat
+step 3 set arr[i] = arr[i-1]
+step 4 set i=i-1
+step 5 end while // repeat step 2 to 5 until condition become false
+step 6 set arr[0] = data 
+step 5 set n=n+1
+step 6 Return
 */
